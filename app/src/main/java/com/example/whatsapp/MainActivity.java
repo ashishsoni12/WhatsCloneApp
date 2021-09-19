0a654c08a1e220ef52cedf7af8cbecdd3496a00f
Amandsharma0002 <amanronik@gmail.com>
2021-09-19 21:19:26 +0530

    checking second commit

--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.whatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -3,12 +3,15 @@ package com.example.whatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 }
